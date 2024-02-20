08700fcb6fddb6258ea7c7506fa8665304a850de
anumtashaikh <anumtaasnain1122@gmail.com>
2024-02-20 20:12:35 +0500

    addedd folders

---
+++ footer.js
@@ -0,0 +1,126 @@
+document.getElementById('Navbar').innerHTML=
+`
+<div id="topbar" class="d-flex align-items-center fixed-top">
+<div class="container d-flex justify-content-between">
+  <div class="contact-info d-flex align-items-center">
+    <i class="bi bi-envelope"></i> <a href="mailto:aliyanahmed853@gmail.com.com">pareesperfume1111@gmail.com</a>
+    <i class="bi bi-phone"></i> +92 323 2752562
+  </div>
+  <div class="d-none d-lg-flex social-links align-items-center">
+    <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+    <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+    <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+    <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+  </div>
+</div>
+</div>
+<header id="header" class="fixed-top">
+<div class="container d-flex align-items-center">
+
+  <h1 class="logo me-auto"><a href="index.html" class="texts">Parees Perfumes</a></h1>
+
+  <nav id="navbar" class="navbar order-last order-lg-0">
+    <ul>
+      <li><a class="nav-link scrollto" href="index.html" class="textt">Home</a></li>
+      <li><a class="nav-link scrollto" href="about us .html" class="textt">About Us</a></li>
+      <li><a class="nav-link scrollto" href="Contact Us.html" class="textt">Contact Us</a></li>
+      <li><a class="nav-link scrollto" href="Sale Item.html" class="textt">Sale</a></li>
+      <li class="dropdown"><a href="#"><span>Brands</span> <i class="bi bi-chevron-down"></i></a>
+        <ul>
+  <li><a  href="Davidoff .html">DAVIDOFF</a></li>
+  <li><a  href="J.Perfumes.html">J.Perfume</a></li>
+  <li><a  href="IDEAS.html">IDEAS</a></li>
+  <li><a  href="BONANZA STRANGI.html">BONANZA STRANGI</a></li>
+  <li><a href="LIMELIGHT.html">LIMELIGHT</a></li>
+  <li><a  href="SAPPHIRE.html">SAPPHIRE</a></li>
+  <li><a  href="MTJ.html">MTJ</a></li>
+  <li><a  href="STYLO.html">STYLO</a></li>
+        </ul>
+       </li> 
+       <li class="dropdown"><a href="#"><span>Categories</span> <i class="bi bi-chevron-down"></i></a>
+        <ul>
+  <li><a  href="Men's.html">Men's</a></li>
+  <li><a  href="Women's.html">Women's</a></li>
+  <li><a  href="Kid's.html">Kid's</a></li>
+  
+ 
+        </ul>
+       </li>
+      <li><a class="nav-link scrollto" href="services.html" class="textt">Services</a></li>
+    </ul>
+    <i class="bi bi-list mobile-nav-toggle"></i>
+  </nav>
+
+  <a href="#" class="appointment-btn scrollto"><span class="d-none d-md-inline">Order Now</span>
+    </a>
+
+</div>
+</header>
+         
+`
+
+document.getElementById('footer').innerHTML=`
+
+<footer class="footer-section p-4   ">
+<div class="container">
+    
+    <div class="footer-content pt-5 pb-5">
+        <div class="row">
+            <div class="col-xl-4 col-lg-4 mb-50">
+                <div class="footer-widget">
+                    <div class="footer-logo">
+                        <div class="logo1">
+                            <h1>Paarees Perfume</h1>
+                        </div>
+                    </div>
+                   
+                    <div class="footer-social-icon">
+                        <span>Follow us</span>
+                        <a href="https://www.facebook.com/"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                        <a href="https://www.twitter.com/"><i class="fab fa-twitter twitter-bg"></i></a>
+                        <a href="https://www.google.com/"><i class="fab fa-google-plus-g google-bg"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                <div class="footer-widget">
+                    <div class="footer-widget-heading">
+                        <h3>Useful Links &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Services</span></h3>
+                    </div>
+                    <ul>
+                        <li><a href="Paarees.html">Home</a></li>
+                        <li><a href="Sale Item.html">Sale</a></li>
+                        <li><a href="about us .html">About Us</a></li>
+                        <li><a href="policy.html">Return Policy</a></li>
+                        <li><a href="Contact Us.html">Contact us</a></li>
+                        <li><a href="terms.html">Term & Condition</a></li>
+                        <li><a href="services.html">Our Services</a></li>
+                        <li><a href="faqs.html">FAQs</a></li>
+                       
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                <div class="footer-widget">
+                    <div class="footer-widget-heading">
+                        <h3>Subscribe</h3>
+                    </div>
+                    <div class="footer-text mb-25">
+                        <p class="text-light">Don't miss to subscribe to our new feeds, kindly fill the form below.</p>
+                    </div>
+                    <div class="subscribe-form">
+                        <form action="#">
+                            <input type="text" placeholder="Email Address" required>
+                            <button><i class="fab fa-telegram-plane"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</footer>
+
+`
+
